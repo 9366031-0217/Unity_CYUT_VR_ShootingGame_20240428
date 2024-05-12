7a1987df93dcf741389b65707725b7bfc9c8d7f8
9366031-0217 <155619075+9366031-0217@users.noreply.github.com>
2024-05-12 11:17:26 +0800

    update 殭屍動畫、攻擊

--- Assets/Scripts/EnemySystem.cs
+++ Assets/Scripts/EnemySystem.cs
@@ -1,15 +1,21 @@
 using UnityEngine;
 using UnityEngine.AI;
 
-public class NewBehaviourScript : MonoBehaviour
+public class EnemySystem : MonoBehaviour
 {
     [SerializeField, Header("玩家")]
     private Transform PlayerPoint;
     [SerializeField, Header("導覽代理器")]
     private NavMeshAgent agent;
+    [SerializeField, Header("動畫控制器")]
+    private Animator ani;
+
+    private string parMove = "移動數值";
 
     private void Update()
     {
         agent.SetDestination(PlayerPoint.position);
+
+        ani.SetFloat(parMove, agent.velocity.magnitude / agent.speed);
     }
 }
---
+++ Assets/Scripts/HpEnemy.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class HpEnemy : HpSystem
+{
+    private string bulletName = "¤l¼u";
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name.Contains(bulletName))
+        {
+            float attack = collision.gameObject.GetComponent<Bullet>().attack;
+            Damage(attack);
+        }
+    }
+    protected override void Dead()
+    {
+        base.Dead();
+        Destroy(gameObject);
+    }
+}
